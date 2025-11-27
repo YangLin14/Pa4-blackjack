194609d9f9da33f225f2971032d60b81959db026
YangLin14 <fongyu93@gmail.com>
2025-11-26 18:37:51 -0800

    pa4 done but need review

--- ai.py
+++ ai.py
@@ -64,7 +64,21 @@ class Agent:
     #Hint: You can keep track the reward of states with this function as well, e.g., as one of the return values
     #Hint: After this function, you can also define another function that simulates one full trajectory, but it's optional
     def make_one_transition(self, action):
-        pass
+        if self.simulator.game_over():
+            return None, 0
+        
+        if action == HIT:
+            self.simulator.act_hit()
+        elif action == STAND:
+            self.simulator.act_stand()
+        
+        reward = self.simulator.check_reward()
+        next_state = self.simulator.state
+        
+        if self.simulator.game_over():
+            next_state = None
+            
+        return next_state, reward
 
     #TODO: Implement MC policy evaluation
     def MC_run(self, num_simulation, tester=False):
@@ -84,6 +98,26 @@ class Agent:
             # Hint: Go through game.py file and figure out which functions will be useful
             # Make sure to update self.MC_values, self.S_MC, self.N_MC for the autograder
             # Don't forget the DISCOUNT
+            episode = []
+            curr_state = self.simulator.state
+            
+            while curr_state is not None:
+                action = self.default_policy(curr_state)
+                
+                next_state, reward = self.make_one_transition(action)
+                
+                episode.append((curr_state, action, reward))
+                curr_state = next_state
+            
+            G = 0
+            for t in reversed(range(len(episode))):
+                state, action, reward = episode[t]
+                
+                G = reward + DISCOUNT * G
+                
+                self.N_MC[state] += 1
+                self.S_MC[state] += G
+                self.MC_values[state] = self.S_MC[state] / self.N_MC[state]
     
     #TODO: Implement TD policy evaluation
     def TD_run(self, num_simulation, tester=False):
@@ -104,6 +138,21 @@ class Agent:
             # Hint: The learning rate alpha is given by "self.alpha(...)"
             # Make sure to update self.TD_values and self.N_TD for the autograder
             # Don't forget the DISCOUNT
+            curr_state = self.simulator.state
+            
+            while curr_state is not None:
+                action = self.default_policy(curr_state)
+                
+                next_state, reward = self.make_one_transition(action)
+                
+                next_val = self.TD_values[next_state] if next_state is not None else 0
+                td_target = reward + DISCOUNT * next_val
+                
+                self.N_TD[curr_state] += 1
+                alpha = self.alpha(self.N_TD[curr_state])
+                self.TD_values[curr_state] += alpha * (td_target - self.TD_values[curr_state])
+                
+                curr_state = next_state
                 
     #TODO: Implement Q-learning
     def Q_run(self, num_simulation, tester=False, epsilon=0.4):
@@ -126,11 +175,37 @@ class Agent:
             # Important: When calling pick_action, use the given parameter epsilon=0.4 to match the autograder
             # Make sure to update self.Q_values, self.N_Q for the autograder
             # Don't forget the DISCOUNT
+            curr_state = self.simulator.state
+            
+            while curr_state is not None:
+                action = self.pick_action(curr_state, epsilon)
+                
+                next_state, reward = self.make_one_transition(action)
+                
+                if next_state is None:
+                    target = reward
+                else:
+                    max_next_q = max(self.Q_values[next_state][HIT], self.Q_values[next_state][STAND])
+                    target = reward + DISCOUNT * max_next_q
+                
+                self.N_Q[curr_state][action] += 1
+                alpha = self.alpha(self.N_Q[curr_state][action])
+                
+                old_q = self.Q_values[curr_state][action]
+                self.Q_values[curr_state][action] = old_q + alpha * (target - old_q)
+                
+                curr_state = next_state
 
     #TODO: Implement epsilon-greedy policy
     def pick_action(self, s, epsilon):
         # TODO: Replace the following random value with an action following the epsilon-greedy strategy
-        return random.randint(0, 1)
+        if random.random() < epsilon:
+            return random.randint(0, 1)
+
+        if self.Q_values[s][HIT] >= self.Q_values[s][STAND]:
+            return HIT
+        else:
+            return STAND
 
     ####Do not modify anything below this line####
 
@@ -177,3 +252,73 @@ class Agent:
         print(f"\r  {name} {i + 1}/{n}", end="")
         if i == n - 1:
             print()
+"""
+I asked Gemini: How exactly do we implement Q-learning and TD(0) in code?
+
+Gemini answered:
+How to Code Q-Learning & TD
+    # Initialize V arbitrarily
+    V = {s: 0.0 for s in env.get_states()}
+
+    for _ in range(episodes):
+        state = env.reset() # Start new game
+        
+        while not env.is_terminal(state):
+            # 1. Pick action based on the policy we are evaluating
+            action = policy[state] 
+            
+            # 2. Take step (Observer R and Next State)
+            next_state, reward = env.step(action)
+            
+            # 3. THE TD UPDATE FORMULA
+            # Target = Reward + Discount * Guess_of_Next_State
+            td_target = reward + gamma * V[next_state]
+            
+            # Update = Old + Alpha * (Target - Old)
+            V[state] = V[state] + alpha * (td_target - V[state])
+            
+            state = next_state
+            
+    return V
+
+Q-LearningUsed for Control (Finding the optimal policy). It is Off-Policy (it learns the optimal value even if it takes random exploration actions).$$Q(s, a) \leftarrow Q(s, a) + \alpha [R + \gamma \max_{a'} Q(s', a') - Q(s, a)]$$Pythonimport random
+
+def q_learning(env, alpha=0.1, gamma=0.9, epsilon=0.1, episodes=1000):
+    # Initialize Q-table: key=(state, action), value=0.0
+    Q = {} 
+    # (Helper to ensure we handle new states safely)
+    def get_q(s, a): return Q.get((s, a), 0.0)
+
+    for _ in range(episodes):
+        state = env.reset()
+        
+        while not env.is_terminal(state):
+            actions = env.get_actions(state)
+            
+            # 1. Epsilon-Greedy Strategy (Exploration vs Exploitation)
+            if random.random() < epsilon:
+                action = random.choice(actions) # Explore
+            else:
+                # Exploit: Choose action with highest current Q
+                action = max(actions, key=lambda a: get_q(state, a))
+            
+            # 2. Take Step
+            next_state, reward = env.step(action)
+            
+            # 3. Find max Q for the NEXT state (Off-Policy lookahead)
+            if env.is_terminal(next_state):
+                max_next_q = 0.0
+            else:
+                next_actions = env.get_actions(next_state)
+                max_next_q = max([get_q(next_state, a) for a in next_actions])
+            
+            # 4. THE Q-LEARNING UPDATE FORMULA
+            current_q = get_q(state, action)
+            target = reward + gamma * max_next_q
+            
+            Q[(state, action)] = current_q + alpha * (target - current_q)
+            
+            state = next_state
+            
+    return Q
+"""
